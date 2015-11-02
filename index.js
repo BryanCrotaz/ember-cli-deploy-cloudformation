3601c6f95f652c15644a27164b6e5ec89efe93e5
Bryan Crotaz <bryan.crotaz@silvercurve.co.uk>
2015-11-02 10:15:54 +0000

    Added stack builder

--- index.js
+++ index.js
@@ -7,16 +7,48 @@ module.exports = {
   createDeployPlugin: function(options) {
     var DeployPlugin = BasePlugin.extend({
       name: options.name,
+      stackName: options.stackName,
+      capabilities: options.capabilities || [],
+      notificationARNs: options.notificationARNs || [],
+      resourceTypes: options.resourceTypes || ['AWS::*'],
       stackParameters: options.stackParams || [],
 
       requiredConfig: [
+      	'stackName'.
       	'accessKeyId', 
       	'secretAccessKey'
       ],
 
       didUpload: function(context) {
         // we want to do this after uploading the assets
-        var cf = AWS.cloudformation();
+        var params = {
+				  StackName: this.stackName, /* required */
+				  Capabilities: this.capabilities,
+				  NotificationARNs: this.notificationARNs,
+				  Parameters: this.stackParameters,
+				  ResourceTypes: this.resourceTypes,
+				};
+				params = params.merge(this._getPolicy(context));
+				params = params.merge(this._getTemplate(context));
+				var cf = AWS.cloudformation();
+				return cf.updateStack(params);
+      },
+
+      _getPolicy: function(context) {
+    		return {
+      		StackPolicyBody: 'STRING_VALUE',
+				  StackPolicyDuringUpdateBody: 'STRING_VALUE',
+				  StackPolicyDuringUpdateURL: 'STRING_VALUE',
+				  StackPolicyURL: 'STRING_VALUE'
+				};
+      },
+
+      _getTemplate: function(context) {
+    		return {
+      		TemplateBody: 'STRING_VALUE',
+				  TemplateURL: 'STRING_VALUE',
+				  UsePreviousTemplate: false
+				};
       }
     });
 
